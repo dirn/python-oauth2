e8a3ab78a787b9834d5175cea3452fdacdb8b88c
Andy Dirnberger <dirn@dirnonline.com>
2013-07-27 17:02:41 -0400

    Remove redundant print
    In b0bc25e I turned the print statement into the print function. Upon
    further thought, print isn't needed at all because the exception that is
    raised immediately after serves the same purpose.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from __future__ import print_function
 from setuptools import setup, find_packages
 import os, re
 
@@ -16,7 +15,6 @@ else:
     if mo:
         mverstr = mo.group(1)
     else:
-        print("unable to find version in %s" % (VERSIONFILE,))
         raise RuntimeError("if %s.py exists, it must be well-formed" % (VERSIONFILE,))
     AVSRE = r"^auto_build_num *= *['\"]([^'\"]*)['\"]"
     mo = re.search(AVSRE, verstrline, re.M)
